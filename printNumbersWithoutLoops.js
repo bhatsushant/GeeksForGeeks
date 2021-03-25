74b517aa61255f41e5338fd6f091d130600d7ddc
Sushant <sush1393@gmail.com>
2021-03-25 13:23:29 +0530

    Added explanatory comments

--- printNumbersWithoutLoops.js
+++ printNumbersWithoutLoops.js
@@ -1,9 +1,8 @@
-
 // Print 1 - 100 without using numbers or loops
 
-const one = +true;
-const zero = +false;
-const hundredString = one.toString() + zero.toString() + zero.toString();
+const one = +true; // This would return 1 (Unary and Boolean operator)
+const zero = +false; // This would return 0 (Unary and Boolean operator)
+const hundredString = one.toString() + zero.toString() + zero.toString(); // Get 100 in string
 
 const printNumbers = (number) => {
     if (number <= parseInt(hundredString)) {
